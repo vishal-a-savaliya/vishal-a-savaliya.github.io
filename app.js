4908d1e8750c1b13263adcb22c81febaeaa16b99
vishalsavaliya <vishandsavaliya@gmail.com>
2021-08-27 20:47:37 +0530

    finish of web

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 
+
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
@@ -14,3 +15,6 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+
